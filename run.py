34a330d4843b05442b3606272edb6a81e3778c05
Zhe He <hezhe88@gmail.com>
2017-02-13 20:41:57 -0500

    Add bootstrap, clean css

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 #!flask/bin/python
 from app import app
+app.secret_key = 'super super secret key'
 
 if __name__ == '__main__':
     try:
-        app.secret_key = 'super secret key'
         # app.run(debug=True, use_reloader=False)
         app.run()
     except (KeyboardInterrupt, SystemExit):
